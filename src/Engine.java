96929daccfd05e7addbcfe67fdb67b976bb4654b
MohandMagdy <92790188+Mohandmagdy@users.noreply.github.com>
2023-11-15 00:01:42 +0200

    Engine.java

--- src/Engine.java
+++ src/Engine.java
@@ -2,27 +2,70 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Engine {
-    private Authentication authentication ;
-    private User currentUser ;
-    private UserFactory userFactory ;
-    Engine(){
+    private Authentication authentication;
+    private User currentUser;
+    private UserFactory userFactory;
+
+    Engine() {
         authentication = Authentication.getInstance();
+        userFactory = new UserFactory();
     }
 
-
-    public void Register(int option){
-        if(option == 1){
-            BankRegisteration b =  new BankRegisteration();
-            //if(b.Register())System.out.println("Account created Successfully");
-            b.dd("honda","hondav1");
-            return;
-        }
-        else if(option==2){
+    private boolean register() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1-Register as BankUser");
+        System.out.println("2-Register as WalletUser");
+        int choice2 = scanner.nextInt();
+        if (choice2 == 1) {
+            BankRegisteration b = new BankRegisteration();
+            if (b.Register()) {
+                System.out.println("Account created Successfully");
+            } else {
+                return false;
+            }
+        } else if (choice2 == 2) {
             new WalletRegisteration().Register();
-            return;
+        }
+        return true;
+    }
+
+    private boolean login() {
+        List<String> userInfo = authentication.login();
+        if (userInfo == null) {
+            System.out.println("Wrong username or password.");
+            return false;
+        }
+        currentUser = userFactory.getCurrentUser(userInfo);
+        return true;
+    }
+
+    private void showServices(){
+        Scanner scanner = new Scanner(System.in);
+        if (currentUser instanceof bankUser){
+            System.out.println("1- transfer to wallet");
+            System.out.println("2- transfer to bank");
+            System.out.println("3- transfer to insta Pay account");
+            System.out.println("4- pay bills");
+            System.out.println("5- logout");
+            int choice = scanner.nextInt();
+            //write response code
+            if(choice == 5){
+                //logout
+            }
+        } else{
+            System.out.println("1- transfer to wallet");
+            System.out.println("2- transfer to insta Pay account");
+            System.out.println("3- pay bills");
+            System.out.println("4- logout");
+            int choice = scanner.nextInt();
+            //write response code
+            if(choice == 4){
+                //logout
+            }
         }
     }
-    public void run(){
+
+    public void run() {
         // display menu
 
         System.out.println("Welcome to Insta Pay Application");
@@ -33,26 +76,18 @@ public class Engine {
             System.out.println("3-Exit");
             int choice = scanner.nextInt();
 
-            if(choice==1){
-
-            }
-
-            else if(choice == 2){
-                System.out.println("1-Register as BankUser");
-                System.out.println("2-Register as WalletUser");
-                int choice2 = scanner.nextInt();
-                Register(choice2);
-            }
-            else {
+            if (choice == 1) {
+                if (login()) {
+                    System.out.println("Welcome " + currentUser.getUsername());
+                    while(true) {
+                        showServices();
+                    }
+                }
+            } else if (choice == 2) {
+                register();
+            } else {
                 break;
             }
-
         }
     }
-
-    void login() {
-        List<String> lst = authentication.login() ;
-        currentUser = userFactory.getCurrentUser(lst);
-    }
-
 }
